31a83716f9528e44c47a4ce037e9235932aba676
Alex <sukhobok.sasha@gmail.com>
2024-01-24 16:07:35 +0100

    created game sprite

--- main.py
+++ main.py
@@ -16,16 +16,31 @@ BG = (0, 0, 0)
 window = pygame.display.set_mode((WIN_WIDTH, WIN_HEIGHT))
 clock = pygame.time.Clock()
 
+class GameSprite(pygame.sprite.Sprite):
+    def __init__ (self, x, y, width, height, picture):
+        super().__init__()
+        self.rect = pygame.Rect(x, y, width, height)
+        self.image = pygame.image.load(file_path(picture))
+        self.image = pygame.transform.scale(self.image, (width, height))
+
+
+    def show(self):
+        window.blit(self.image, self.rect)
+
+
+player = GameSprite(400, 500, BLOCK, BLOCK, r"images\I 1.png")
+
 game = True
 level = 1
 while game:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
-            
             game = False
 
     if level == 1:
         window.fill(BG)
+
+        player.show()
 
     clock.tick(FPS)
     pygame.display.update()
