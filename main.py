686d1b4ce2e08c9909d47eb6df710a116336558f
Alex <sukhobok.sasha@gmail.com>
2024-01-30 15:24:22 +0100

    added player sprite group

--- main.py
+++ main.py
@@ -21,14 +21,21 @@ class GameSprite(pygame.sprite.Sprite):
         super().__init__()
         self.rect = pygame.Rect(x, y, width, height)
         self.image = pygame.image.load(file_path(picture))
-        self.image = pygame.transform.scale(self.image, (width, height))
-
+        self.image = pygame.transform.scale(self.image, (width, height)) 
 
     def show(self):
         window.blit(self.image, self.rect)
 
+
+class Player(GameSprite):
+    def __init__(self, x, y, width, height, picture):
+        super().__init__(x, y, width, height, picture)
+
+    def update(self):
+        pass 
+
 map_1 = [
-    "   0000          000               ",
+    "   0000     1     000              ",
     "                                   ",
     "                                   ",
     "                                   ",
@@ -52,22 +59,28 @@ map_1 = [
 
 '''
 0 - platform
+1 - player
 '''
 platforms = pygame.sprite.Group()
+player = pygame.sprite.Group()
 
 def create_level(lvl):
     platforms.empty()
-
+    player.empty()
+    
     for row in range(len(lvl)):
         for col in range(len(lvl[row])):
             if lvl[row][col] == "0":
                 obj = GameSprite(col*BLOCK, row*BLOCK, BLOCK, BLOCK, r"images\ground 1.png")
                 platforms.add(obj)
 
+            elif lvl[row][col] == "1":
+                obj = Player(col*BLOCK, row*BLOCK, BLOCK, BLOCK, r"images\I 1.png")
+                player.add(obj)
+
 create_level(map_1)
 
 
-player = GameSprite(400, 500, BLOCK, BLOCK, r"images\I 1.png")
 
 game = True
 level = 1
@@ -80,7 +93,9 @@ while game:
         window.fill(BG)
 
         platforms.draw(window)
-        player.show()
+        player.draw(window)
+
+        player.update()
 
     clock.tick(FPS)
     pygame.display.update()
